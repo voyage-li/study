b0093425c2bd3a736f0e15cf62c57750c168f65a
voyage <923959605@qq.com>
2022-01-22 01:59:42 +0800

    HW01

---
+++ HW/HW_01/q2.py
@@ -0,0 +1,27 @@
+from operator import add, sub
+
+
+def a_plus_abs_b(a, b):
+    """Return a*a + b*b, where a and b are the two smallest members of the
+    positive numbers x, y, and z.
+
+    >>> two_of_three(1, 2, 3)
+    5
+    >>> two_of_three(5, 3, 1)
+    10
+    >>> two_of_three(10, 2, 8)
+    68
+    >>> two_of_three(5, 5, 5)
+    50
+    # check that your code consists of nothing but an expression (this docstring)
+    >>>
+    >>> # a return statement
+    >>> import inspect, ast
+    >>> [type(x).__name__ for x in ast.parse(inspect.getsource(two_of_three)).body[0].body]
+    ['Expr', 'Return']
+    """
+    if b >= 0:
+        h = add
+    else:
+        h = sub
+    return h(a, b)
---
+++ HW/HW_01/q4.py
@@ -0,0 +1,15 @@
+def largest_factor(x):
+    """Return the largest factor of x that is smaller than x.
+
+    >>> largest_factor(15) # factors are 1, 3, 5
+    5
+    >>> largest_factor(80) # factors are 1, 2, 4, 5, 8, 10, 16, 20, 40
+    40
+    >>> largest_factor(13) # factor is 1 since 13 is prime
+    1
+    """
+    "*** YOUR CODE HERE ***"
+    for i in range(x-1, 0, -1):
+        if(x % i == 0):
+            return i
+    return 1
